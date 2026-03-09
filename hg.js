6cadf576e3157b1f9d54c16fe511382cd240f176
shawn-mcgee <s.mcgeek07@gmail.com>
2026-03-09 14:43:02 -0600

    slowly

--- hg.js
+++ hg.js
@@ -53,70 +53,72 @@ const VERSION = Version.new({
  * @typedef {number | number[]} hgVectorLike
  */
 
-const X = 0;
-const Y = 1;
-const Z = 2;
-const W = 3;
-
-const Vector__get__ = {
-  /** @param {hgVectorLike} a */
-  x(a) { return typeof a === "number" ? a : a[X] ?? 0 },
-  /** @param {hgVectorLike} a */
-  y(a) { return typeof a === "number" ? a : a[Y] ?? 0 },
-  /** @param {hgVectorLike} a */
-  z(a) { return typeof a === "number" ? a : a[Z] ?? 0 },
-  /** @param {hgVectorLike} a */
-  w(a) { return typeof a === "number" ? a : a[W] ?? 0 },
-  /** @param {hgVectorLike} a */
-  n(a) { return typeof a === "number" ? 1 : a.length ?? 0 },
-}
-
-const Vector__set__ = {
-  /** 
-   * @param {hgVectorLike} a 
-   * @param {number} x
-   */
-  x(a, x) { return typeof a === "number" ? x : (a[X] = x) },
-  /** 
-   * @param {hgVectorLike} a 
-   * @param {number} y
-   */
-  y(a, y) { return typeof a === "number" ? y : (a[Y] = y) },
-  /** 
-   * @param {hgVectorLike} a 
-   * @param {number} z
-   */
-  z(a, z) { return typeof a === "number" ? z : (a[Z] = z) },
-  /** 
-   * @param {hgVectorLike} a 
-   * @param {number} w
-   */
-  w(a, w) { return typeof a === "number" ? w : (a[W] = w) },
-}
-
 const Vector = {
-  __get__: Vector__get__,
-  __set__: Vector__set__,
+  /** @type {0} */
+  X: 0,
+  /** @type {1} */
+  Y: 1,
+  /** @type {2} */
+  Z: 2,
+  /** @type {3} */
+  W: 3,
+
+  __get__: {
+    /** @param {hgVectorLike} a */
+    x(a) { return typeof a === "number" ? a : a[Vector.X] ?? 0 },
+    /** @param {hgVectorLike} a */
+    y(a) { return typeof a === "number" ? a : a[Vector.Y] ?? 0 },
+    /** @param {hgVectorLike} a */
+    z(a) { return typeof a === "number" ? a : a[Vector.Z] ?? 0 },
+    /** @param {hgVectorLike} a */
+    w(a) { return typeof a === "number" ? a : a[Vector.W] ?? 0 },
+    /** @param {hgVectorLike} a */
+    n(a) { return typeof a === "number" ? 1 : a.length ?? 0 },
+  },
+
+  __set__: {
+    /** 
+     * @param {hgVectorLike} a 
+     * @param {number} x
+     */
+    x(a, x) { return typeof a === "number" ? x : (a[Vector.X] = x) },
+    /** 
+     * @param {hgVectorLike} a 
+     * @param {number} y
+     */
+    y(a, y) { return typeof a === "number" ? y : (a[Vector.Y] = y) },
+    /** 
+     * @param {hgVectorLike} a 
+     * @param {number} z
+     */
+    z(a, z) { return typeof a === "number" ? z : (a[Vector.Z] = z) },
+    /** 
+     * @param {hgVectorLike} a 
+     * @param {number} w
+     */
+    w(a, w) { return typeof a === "number" ? w : (a[Vector.W] = w) },
+  },
+
   /**
    * @param {hgVectorLike} a
    * @param {number} [x]
    */
-  x(a, x) { return x === undefined ? Vector__get__.x(a) : Vector__set__.x(a, x) },
+  x(a, x) { return x === undefined ? Vector.__get__.x(a) : Vector.__set__.x(a, x) },
   /**
    * @param {hgVectorLike} a
    * @param {number} [y]
    */
-  y(a, y) { return y === undefined ? Vector__get__.y(a) : Vector__set__.y(a, y) },
+  y(a, y) { return y === undefined ? Vector.__get__.y(a) : Vector.__set__.y(a, y) },
   /**
    * @param {hgVectorLike} a
    * @param {number} [z]
    */
-  z(a, z) { return z === undefined ? Vector__get__.z(a) : Vector__set__.z(a, z) },
+  z(a, z) { return z === undefined ? Vector.__get__.z(a) : Vector.__set__.z(a, z) },
   /**
    * @param {hgVectorLike} a
    * @param {number} [w]
    */
-  w(a, w) { return w === undefined ? Vector__get__.w(a) : Vector__set__.w(a, w) },
+  w(a, w) { return w === undefined ? Vector.__get__.w(a) : Vector.__set__.w(a, w) },
 }
 
 /** @typedef {[number, number]} hgVector2 */
@@ -231,6 +233,293 @@ const Canvas = {
     delete  Canvas.__default__
     return (Canvas.__default__ = c)
   }
+}
+
+/**
+ * @template T
+ * @typedef {string & { __kind__: T }} hgId
+*/
+
+const Id = {
+  /** @type {Record<hgId<any>, any>} */
+  __table__: { },
+
+  /**
+   * @template T
+   * @param {T} what
+   * @param {string} [id]
+   */
+  acquire(what, id) {
+    id ??= Id.__uniqueId__()
+    if (id in Id.__table__)
+      throw new Error(`[Id.acquire] item with id '${id}' already exists.`)
+    Id.__table__[id] = what
+    return id
+  },
+
+  /**
+   * @template T
+   * @param {hgId<T>} id
+   * @returns {T}
+   */
+  resolve(id) {
+    if (!(id in Id.__table__))
+      throw new Error(`[Id.resolve] item with id '${id}' does not exist.`)
+    return Id.__table__[id]
+  },
+
+  /**
+   * @template T
+   * @param {hgId<T>} id
+   */
+  release(id) {
+    if (!(id in Id.__table__))
+      throw new Error(`[Id.release] item with id '${id}' does not exist.`)
+    delete Id.__table__[id]
+  },
+
+  __uniqueId__() {
+    let id = crypto.randomUUID()
+    while (id in Id.__table__) 
+        id = crypto.randomUUID()
+    return id
+  }
+}
+
+const LISTEN   = "__listen__"
+const DEAFEN   = "__deafen__"
+const DISPATCH = "__dispatch__"
+/**
+ * @template T
+ * @typedef {(what: T, context: hgEventContext<T>) => void} hgEventHandler
+ */
+
+/**
+ * @template T
+ * @typedef {hgEventHandler<T>[]} hgEventHandlers
+ */
+
+/**
+ * @typedef hgEventNode
+ * @property {Record<string, hgEventHandlers<any>>} handlers
+ * @property {Record<string, hgEventNode>         } children
+ */
+
+/**
+ * @typedef hgEventTree
+ * @property {hgEventNode         } root
+ * @property {hgEventAction<any>[]} pending
+ */
+
+/**
+ * @template T
+ * @typedef hgEventContext
+ * @property {hgEventTree            } tree
+ * @property {hgEventNode            } node
+ * @property {string                 } path
+ * @property {string                 } when
+ * @property {hgId<hgEventHandler<T>>} self
+ */
+
+/**
+ * @template T
+ * @typedef hgEventAction__Listen__
+ * @property {typeof LISTEN          } action
+ * @property {string                 } path
+ * @property {string                 } when
+ * @property {hgId<hgEventHandler<T>>} then
+ */
+
+/**
+ * @template T
+ * @typedef hgEventAction__Deafen__
+ * @property {typeof DEAFEN          } action
+ * @property {string                 } path
+ * @property {string                 } when
+ * @property {hgId<hgEventHandler<T>>} then
+ */
+
+/**
+ * @template T
+ * @typedef hgEventAction__Dispatch__
+ * @property {typeof DISPATCH} action
+ * @property {string         } path
+ * @property {string         } when
+ * @property {T              } what
+ */
+
+/**
+ * @template T
+ * @typedef {hgEventAction__Listen__<T> | hgEventAction__Deafen__<T> | hgEventAction__Dispatch__<T>} hgEventAction
+ */
+
+const Event = {
+
+  Node: {
+    /** @returns {hgEventNode} */
+    new() {
+      return {
+        handlers: { },
+        children: { },
+      }
+    },
+
+    /**
+     * @param {hgEventNode | undefined} root
+     * @param {string                 } path
+     */
+    __requestNode__(root, path) {
+      for (let part of path.split("/")) {
+        let node = root?.children?.[part]
+        if (!node) return
+        root = node
+      }
+      return root
+    },
+
+    /**
+     * 
+     * @param {hgEventNode} root 
+     * @param {string     } path 
+     */
+    __requireNode__(root, path) {
+      for (let part of path.split("/")) {
+        let node = root.children[part]
+        if (!node) root.children[part] = (
+          node = Event.Node.new()
+        )
+        root = node
+      }
+      return root
+    },
+
+    /**
+     * @param {hgEventNode | undefined} root 
+     * @param {string                 } when 
+     */
+    __requestHandlers__(root, when) {
+      let list = root?.handlers?.[when]
+      if (!list) return
+      return list
+    },
+
+    /**
+     * @param {hgEventNode} root 
+     * @param {string     } when 
+     */
+    __requireHandlers__(root, when) {
+      let list = root.handlers[when]
+      if (!list) root.handlers[when] = (
+        list = [ ]
+      )
+      return list
+    },
+
+    __releaseNode__(root) {
+
+
+    },
+  },
+
+  Tree: {
+    /** @returns {hgEventTree} */
+    new() {
+      return {
+        root: Event.Node.new(),
+        pending: [ ],
+      }
+    },
+
+    /**
+     * @template T
+     * @param {hgEventTree      } tree
+     * @param {string           } when 
+     * @param {hgEventHandler<T>} then
+     * @param {{path ?: string, defer?: boolean}}
+     */
+    listen(tree, when, then, {path, defer}={}) {
+
+    },
+
+    /**
+     * @template T
+     * @param {hgEventTree            } tree
+     * @param {string                 } when 
+     * @param {hgId<hgEventHandler<T>>} then
+     * @param {{path ?: string, defer?: boolean}}
+     */
+    deafen(tree, when, then, {path, defer}={}) {
+
+    },
+
+    /**
+     * @template T
+     * @param {hgEventTree} tree
+     * @param {string     } when
+     * @param {T          } what
+     * @param {{path ?: string, defer?: boolean}}
+     */
+    dispatch(tree, when, what, {path, defer}={}) {
+
+    },
+
+    /** @param {hgEventTree} tree */
+    poll(tree) {
+      tree.pending.splice(0).forEach(
+        a => Event.__flush__(tree, a)
+      )
+    },
+
+    /** 
+     * @param {hgEventTree       } tree
+     * @param {hgEventAction<any>} a
+     */
+    __queue__(tree, a) {
+      tree.pending.push(a)
+    },
+
+    /** 
+     * @param {hgEventTree       } tree
+     * @param {hgEventAction<any>} a
+     */
+    __flush__(tree, a) {
+      switch (a.action) {
+        case LISTEN  : Event.__onListen__  (tree, a); break;
+        case DEAFEN  : Event.__onDeafen__  (tree, a); break;
+        case DISPATCH: Event.__onDispatch__(tree, a); break;
+        default: throw new Error(`[Event.flush] unknown action '${a.action}'.`)
+      }
+    },
+
+    /** 
+     * @param {hgEventTree                 } tree
+     * @param {hgEventAction__Listen__<any>}
+     */
+    __onListen__  (tree, a) {
+      const node = Event.Node.__requireNode__(tree.root, a.path)
+      const list = Event.Node.__requireHandlers__(node , a.when)
+      if (list.includes(a.then))
+        console.warn(`[Event.listen] handler with id '${a.then}' already exists.`)
+      list.push(a.then)
+    },
+
+    /** 
+     * @param {hgEventTree                 } tree
+     * @param {hgEventAction__Deafen__<any>} a
+     */
+    __onDeafen__  (tree, a) {
+      
+
+    },
+
+    /** 
+     * @param {hgEventTree                   } tree
+     * @param {hgEventAction__Dispatch__<any>} a
+     */
+    __onDispatch__(tree, a) {
+
+    }
+  },
 }
 
 /**
@@ -369,29 +658,11 @@ function Stage__loop__(stage, t0, t1, t2) {
 }
 
 
-const Event = {
 
-}
 
-function Event__queue__(bus, a) {
 
-}
 
-function Event__flush__(bus, a) {
 
-}
-
-function Event__onListen__  (bus, a) {
-
-}
-
-function Event__onDeafen__  (bus, a) {
-
-}
-
-function Event__onDispatch__(bus, a) {
-
-}
 
 const hg = {
   Version,
@@ -402,6 +673,8 @@ const hg = {
   Vector3,
   Vector4,
 
+  Id,
+  Event,
   Stage,
 }
 
